dc5cf18bc2b03fec1ba559330076167358acb9d9
Nadd_co Amado <64038165+danielAcl9@users.noreply.github.com>
2022-05-19 15:46:07 +0000

    Añadido las frases motivacionales

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ import discord
 
 import glob, random
 
+import requests, json
+
 #Se crea una instancia del cliente, esta es la conexión con Discord. 
 #Es parte de la librería de discord.py
 client = discord.Client()
 
+def get_quote():
+  response = requests.get('https://zenquotes.io/api/random')
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return (quote)
+  
+
 #Registrar un evento
 @client.event
-
 #Este evento se llamará cuando el bot esté listo para usarse
 async def on_ready(): 
   print('Iniciado como {0.user}'.format(client))
@@ -21,9 +29,16 @@ async def on_message(message):
   if message.author == client.user:
     return
 
+  #Mensaje de saludo
   if message.content.startswith('$hello'):
     await message.channel.send('Hello!')
 
+  #Mensaje de saludo
+  if message.content.startswith('$frase'):
+    frase = get_quote()
+    await message.channel.send(frase)
+
+  #Generar una imagen aleatoria
   if message.content.startswith('$img'):
     file_path_type = ['./Fuente/*.png', './Fuente/*.jpeg', './Fuente/*.jpg']
     images = glob.glob(random.choice(file_path_type))
