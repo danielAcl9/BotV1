0373aacf93c6d71f1a81d9f9c9f683069aa186aa
Nadd_co Amado <64038165+danielAcl9@users.noreply.github.com>
2022-05-19 15:13:50 +0000

    Opción para imagen aleatoria

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import discord
 
+import glob, random
+
 #Se crea una instancia del cliente, esta es la conexión con Discord. 
 #Es parte de la librería de discord.py
 client = discord.Client()
@@ -21,5 +23,15 @@ async def on_message(message):
 
   if message.content.startswith('$hello'):
     await message.channel.send('Hello!')
+
+  if message.content.startswith('$img'):
+    file_path_type = ['./Fuente/*.png', './Fuente/*.jpeg', './Fuente/*.jpg']
+    images = glob.glob(random.choice(file_path_type))
+    random_image = random.choice(images)
+
+    
+    await message.channel.send(file=discord.File(random_image))
+
+
 
 client.run(os.getenv('discToken'))
